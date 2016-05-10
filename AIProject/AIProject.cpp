f4ac025c8ac1d569730ba926ab42c5e9e4b3b796
callumjones17 <s3601120@student.rmit.edu.au>
2016-05-10 17:15:28 +1000

    first commit

---
+++ AIProject/AIProject.cpp
@@ -0,0 +1,15 @@
+// AIProject.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+
+	return 0;
+}
+
